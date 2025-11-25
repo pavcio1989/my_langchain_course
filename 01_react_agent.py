95e0cf48a67a20b018d38d401cd6c80397631849
pawel <pavcio1989@gmail.com>
2025-11-25 12:30:06 +0100

    case 1 react agent with tavily search tool

--- 01_react_agent.py
+++ 01_react_agent.py
@@ -4,31 +4,16 @@ from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 
 load_dotenv()
-
-tavily = TavilyClient()
-
-
-@tool
-def search(query: str):
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Return:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
 
 
 if __name__ == '__main__':
     
     llm = ChatOpenAI(temperature=0, model='gpt-4.1')
-    tools = [search]
+    tools = [TavilySearch()]
     agent = create_agent(model=llm, tools=tools)
 
     result = agent.invoke({"messages": HumanMessage(content="Search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
